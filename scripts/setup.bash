15e7293040673cfc28db5f06490879349fd4faa9
Rafael Coelho <rafael.coelho.x@gmail.com>
2023-05-23 21:20:34 -0300

    better setup and using env var on fluent bit config

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-# Installing Elastic CDR
-kubectl create -f https://download.elastic.co/downloads/eck/2.7.0/crds.yaml
-kubectl apply -f https://download.elastic.co/downloads/eck/2.7.0/operator.yaml
+# Setup namespaces
+kubectl apply -f k8s/namespace.yml
 
 # Installing Cert Manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
-
-# Istalling Opentelementry CDR
-kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
+kubectl wait --for=condition=ready pod -l app=webhook -n cert-manager
 
 # Installing Istio
 istioctl install
 
+# Installing Elastic CDR
+kubectl create -f https://download.elastic.co/downloads/eck/2.7.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.7.0/operator.yaml
+
+# Istalling Opentelementry CDR
+kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
+
 # Installing fluentbit
-helm upgrade --install fluent-bit fluent/fluent-bit -f helm/fluentbit/values.yml
-
-
-# Setup namespaces
-helm upgrade --install fluent-bit fluent/fluent-bit -f helm/fluentbit/values.yml
+helm upgrade -n observability --install fluent-bit fluent/fluent-bit -f helm/fluentbit/values.yml 
 
 # Setup apps, observability, etc.
 kubectl apply -f k8s -R
 
-# sleep 15
-
+# Opentelemetry Operator
 kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
 
