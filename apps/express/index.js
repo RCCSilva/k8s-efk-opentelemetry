8dab6c2bfed8cafe0374921e16ecaceb2e1c4a53
Rafael Coelho <rafael.coelho.x@gmail.com>
2023-05-23 22:22:10 -0300

    better on logging on example apps

--- apps/express/index.js
+++ apps/express/index.js
@@ -38,7 +38,7 @@ const asyncLoop = async () => {
   const api = axios.create({ baseURL: 'http://app-fastify.dev.svc:3000/' })
   for await (const _ of setInterval(1000)) {
     const response = await api.get('/').catch(err => logger.error(err))
-    logger.info(`${response.status} ${response.data}`)
+    logger.info(`${response.status} ${JSON.stringify(response.data)}`)
   }
 }
 
--- apps/fastify/index.js
+++ apps/fastify/index.js
@@ -5,8 +5,17 @@ apm.start({ captureBody: 'transactions' })
 
 const { randomUUID } = require('crypto');
 
+const winston = require('winston');
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  defaultMeta: {
+    'service.name': 'app-fastify'
+  }
+})
+
 const { default: axios } = require('axios');
 const fastify = require('fastify')
+
 
 // Require the framework and instantiate it
 const app = fastify({
@@ -16,6 +25,7 @@ const app = fastify({
 // Declare a route
 app.get('/', async (request, reply) => {
   await axios.get('http://app-express.dev.svc:3000/', { headers: { customLabel: randomUUID() } })
+  logger.info(`random message from ${request.ip}`)
   reply.send({ hello: 'world' })
 })
 
